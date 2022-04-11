64d846358ca9c6894b21b11b2e1331ab1a14d4dd
facuzylberberg <101568863+facuzylberberg@users.noreply.github.com>
2022-04-11 20:44:09 -0300

    ej06 finished

--- Assets/Validar.cs
+++ Assets/Validar.cs
@@ -9,10 +9,14 @@ public class Validar : MonoBehaviour
     string contraseniaCorrecta;
     string contraseniaUsuario;
     public Text ingresoUsuario;
+    public GameObject cartel;
+    public Text textomsj;
+
     // Start is called before the first frame update
     void Start()
     {
         contraseniaCorrecta = "12345";
+        cartel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,10 +30,14 @@ public class Validar : MonoBehaviour
         contraseniaUsuario = ingresoUsuario.text;
         if(contraseniaUsuario == contraseniaCorrecta)
         {
+            cartel.SetActive(true);
+            textomsj.text = "¡Bienvenido!";
             Debug.Log("¡Bienvenido!");
         }
         else
         {
+            cartel.SetActive(true);
+            textomsj.text = "Contraseña incorrecta";
             Debug.Log("Contraseña incorrecta");
         }
     }
