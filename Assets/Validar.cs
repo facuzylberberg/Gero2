dd84496024eaa5f2837a98df90f2c284c4cf6380
facuzylberberg <101568863+facuzylberberg@users.noreply.github.com>
2022-04-11 20:26:19 -0300

    ej04,05 finished

---
+++ Assets/Validar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Validar : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if(contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("¡Bienvenido!");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
+
